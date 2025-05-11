abd66c6ce5991e685da28d2bd690757dc61089c7
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-05-11 14:36:48 -0400

    Test build and deploy instructions

--- sheep-dog-specs/scripts/forward-engineer.bat
+++ sheep-dog-specs/scripts/forward-engineer.bat
@@ -1,6 +1,6 @@
 cd ..
 call mvn clean
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:asciidoctor-to-uml -Dtags="sheep-dog-dev" -DrepoDir=""
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:asciidoctor-to-uml -Dtags="sheep-dog-test" -DrepoDir=""
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:asciidoctor-to-uml -Dtags="round-trip" -DrepoDir=""
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:asciidoctor-to-uml -DrepoDir="" -Dtags="sheep-dog-dev"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:asciidoctor-to-uml -DrepoDir="" -Dtags="sheep-dog-test"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.26-SNAPSHOT:asciidoctor-to-uml -DrepoDir="" -Dtags="round-trip"
 cd scripts 
