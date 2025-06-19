5dfb486a99f1a9dfc364cd2cb8ee303e0e5c4754
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-06-18 21:31:14 -0400

    Setup e2e testing with AWS

--- sheep-dog-specs/scripts/forward-engineer-svc.bat
+++ sheep-dog-specs/scripts/forward-engineer-svc.bat
@@ -3,5 +3,4 @@ call mvn clean
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.29-SNAPSHOT:asciidoctor-to-uml -Dtags="sheep-dog-dev"
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.29-SNAPSHOT:asciidoctor-to-uml -Dtags="sheep-dog-test"
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.29-SNAPSHOT:asciidoctor-to-uml -Dtags="round-trip"
-call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.29-SNAPSHOT:uml-to-cucumber-guice -Dtags="round-trip"
 cd scripts 
