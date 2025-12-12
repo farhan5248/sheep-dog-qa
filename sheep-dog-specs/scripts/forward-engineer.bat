7165d45e8fef59aeaccabfe724152726ae563c2a
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-12-12 16:42:35 -0500

    Split step name into "object,predicate" in the xtext grammar

--- sheep-dog-specs/scripts/forward-engineer.bat
+++ sheep-dog-specs/scripts/forward-engineer.bat
@@ -1,6 +1,6 @@
 cd ..
 call mvn clean
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.30:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-dev"
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.30:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-test"
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.30:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="round-trip"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.31-SNAPSHOT:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-dev"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.31-SNAPSHOT:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-test"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.31-SNAPSHOT:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="round-trip"
 cd scripts 
