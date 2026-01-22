9cbc64e5185929ba192882be8d229d8db61cc402
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-01-22 00:08:46 -0500

    Create claude skills to reverse engineer tests

--- sheep-dog-specs/scripts/reverse-engineer-local.bat
+++ sheep-dog-specs/scripts/reverse-engineer-local.bat
@@ -1,4 +1,3 @@
 cd ..
-call mvn clean
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.32:uml-to-asciidoctor -Dtags="%1"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.35:uml-to-asciidoctor -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="%1"
 cd scripts 
--- sheep-dog-specs/scripts/reverse-engineer.bat
+++ sheep-dog-specs/scripts/reverse-engineer.bat
@@ -1,4 +1,3 @@
 cd ..
-call mvn clean
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:uml-to-asciidoctor -Dtags="%1"
 cd scripts 
