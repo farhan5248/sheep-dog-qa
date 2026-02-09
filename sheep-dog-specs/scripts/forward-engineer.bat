5cdfd25e51b323d171b2b5172899caed287cb28a
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-02-09 12:44:43 -0500

    Split TypeUtility into TypeFragmentUtility and TypeListUtility

--- sheep-dog-specs/scripts/forward-engineer.bat
+++ sheep-dog-specs/scripts/forward-engineer.bat
@@ -3,5 +3,4 @@ call mvn clean
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="sheep-dog-dev" -Dhost="dev.sheepdogdev.io"
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="sheep-dog-test" -Dhost="dev.sheepdogdev.io"
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="round-trip" -Dhost="dev.sheepdogdev.io"
-call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="single-test" -Dhost="dev.sheepdogdev.io"
 cd scripts 
--- sheep-dog-specs/scripts/reverse-engineer-local.bat
+++ sheep-dog-specs/scripts/reverse-engineer-local.bat
@@ -1,3 +1,3 @@
 cd ..
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.35:uml-to-asciidoctor -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="%1"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.37:uml-to-asciidoctor -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="%1"
 cd scripts 
