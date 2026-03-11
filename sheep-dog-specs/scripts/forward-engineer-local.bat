1dc8d7af2cec0362d39ce33a1c7830f07c6ae24e
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-03-11 17:03:19 -0400

    Review negativeTest flag

--- sheep-dog-specs/scripts/forward-engineer-local.bat
+++ sheep-dog-specs/scripts/forward-engineer-local.bat
@@ -1,6 +1,5 @@
 cd ..
 call mvn clean
 call mvn org.farhan:sheep-dog-dev-maven-plugin:1.46:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-dev"
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.46:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-test"
 call mvn org.farhan:sheep-dog-dev-maven-plugin:1.46:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="round-trip"
 cd scripts 
