2c4362ba56b400e8cc675985382fb14db1f603a5
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-03-22 20:09:41 -0400

    Upgrade plugins

--- sheep-dog-specs/scripts/forward-engineer-local.bat
+++ sheep-dog-specs/scripts/forward-engineer-local.bat
@@ -1,5 +1,5 @@
 cd ..
 call mvn clean
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.46:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-dev"
-call mvn org.farhan:sheep-dog-dev-maven-plugin:1.46:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="round-trip"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.47:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="sheep-dog-dev"
+call mvn org.farhan:sheep-dog-dev-maven-plugin:1.47:asciidoctor-to-uml -DrepoDir="../../sheep-dog-qa/sheep-dog-specs/" -Dtags="round-trip"
 cd scripts 
