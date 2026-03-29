8530743d40dcfcacfb59c3e65d0411ded94fc4e9
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-03-28 21:11:12 -0400

    Split sheep-dog-dev-svc into api and code-gen services

--- sheep-dog-specs/scripts/forward-engineer.bat
+++ sheep-dog-specs/scripts/forward-engineer.bat
@@ -1,5 +1,6 @@
 cd ..
 call mvn clean
+call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="sheep-dog-dev" -Dhost="dev.sheepdogdev.io"
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="sheep-dog-grammar" -Dhost="dev.sheepdogdev.io"
 call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:asciidoctor-to-uml -Dtags="round-trip" -Dhost="dev.sheepdogdev.io"
-cd scripts 
+cd scripts
